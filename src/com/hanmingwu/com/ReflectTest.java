d6d62abb199bd73a5e18204a22e6b35f72aec051
hanmingwu <37945219+yourena315@users.noreply.github.com>
2019-10-30 22:16:09 +0800

    第10天

--- src/com/hanmingwu/com/ReflectTest.java
+++
@@ -1,8 +0,0 @@
-package com.hanmingwu.com;
-
-public class ReflectTest {
-    public static void main(String[] args) {
-        System.out.println("测试");
-        System.out.println("再次测试");
-    }
-}
---
+++ src/com/hanmingwu/www/XmLTest.java
@@ -0,0 +1,5 @@
+package com.hanmingwu.www;
+
+public class XmLTest {
+
+}
